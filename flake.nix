e444d54b9696cbbf5cc0fff0e356b7df248fd2af
Philipp Schuster <phip1611@gmail.com>
2025-03-17 15:35:46 +0100

    flake: init

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "uefi-rs";
+
+  inputs = {
+    # We follow the latest stable release of nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+  };
+
+  outputs =
+    inputs@{ self, nixpkgs, ... }:
+    let
+      # Systems definition for dev shells and exported packages,
+      # independent of the NixOS configurations and modules defined here. We
+      # just use "every system" here to not restrict any user. However, it
+      # likely happens that certain packages don't build for/under certain
+      # systems.
+      systems = nixpkgs.lib.systems.flakeExposed;
+      forAllSystems =
+        function: nixpkgs.lib.genAttrs systems (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      formatter = forAllSystems (pkgs: pkgs.nixfmt-rfc-style);
+      packages = forAllSystems (pkgs: {
+        default = pkgs.rustPlatform.buildRustPackage rec {
+          src = pkgs.nix-gitignore.gitignoreSource [ ] ./.;
+          name = "fcntl-tool";
+          cargoDeps = pkgs.rustPlatform.importCargoLock {
+            lockFile = ./Cargo.lock;
+          };
+        };
+      });
+    };
+}
